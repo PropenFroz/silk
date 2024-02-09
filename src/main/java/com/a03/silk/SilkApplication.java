297dbea355d393605d4c4ea27193fca9cc1d2dc1
shafanjw <shafa.najwa@ui.ac.id>
2024-02-09 18:14:35 +0700

    first init

---
+++ src/main/java/com/a03/silk/SilkApplication.java
@@ -0,0 +1,13 @@
+package com.a03.silk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SilkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SilkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/a03/silk/SilkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.a03.silk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SilkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

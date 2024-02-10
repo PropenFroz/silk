f0c384f3ee42e13788c7fa8fb1c62fb9e052bb8c
shafanjw <shafa.najwa@ui.ac.id>
2024-02-10 20:46:11 +0700

    add react frontend

--- src/main/java/com/a03/silk/Application.java
+++ src/main/java/com/a03/silk/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SilkApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SilkApplication.class, args);
+		SpringApplication.run(Application.class, args);
+		System.out.println("------------------ APPLICATION SUCCESSFULLY STARTED ------------------");
 	}
-
 }

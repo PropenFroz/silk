55293bbafad2a7b2c3398a8d9bde512670782af4
shafanjw <shafa.najwa@ui.ac.id>
2024-05-05 21:06:53 +0700

    Hapus Akun Guru, fix guru fetch (reset DB to create)

--- src/main/java/com/a03/silk/repository/EntryGajiGuruDetailDb.java
+++ src/main/java/com/a03/silk/repository/EntryGajiGuruDetailDb.java
@@ -13,6 +13,7 @@ import com.a03.silk.model.Guru;
 public interface EntryGajiGuruDetailDb extends JpaRepository<EntryGajiGuruDetail, Long> {
 
     List<EntryGajiGuruDetail> findByEntryGajiGuru_GuruAndTanggalBetween(Guru guru, Date startDate, Date endDate);
-    
+
+
     List<EntryGajiGuruDetail> findByTanggalBetweenOrderByTanggalAsc(Date startDate, Date endDate);
 }
